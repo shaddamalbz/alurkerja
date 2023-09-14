44c9efd91cfdcafdbaff990f2d709baaaf465e46
shaddamalbz <shaddam.albz@gmail.com>
2023-09-14 15:50:52 +0700

    style: adjust switch width

--- packages/alurkerja-ui/src/components/Switch/Switch.tsx
+++ packages/alurkerja-ui/src/components/Switch/Switch.tsx
@@ -30,7 +30,7 @@ export const Switch: FC<SwitchProps> = ({ options, onChange, defaultValue = fals
       <div className="w-full flex items-center">
         {options.map((option, idx: number) => (
           <div
-            className={`w-[55px] text-white py-2 flex justify-center cursor-pointer ${
+            className={`w-fit text-white p-2 flex justify-center cursor-pointer ${
               selected === option.value ? (idx % 2 === 0 ? 'bg-[#005FC2]' : ' bg-[#F4402C]') : 'bg-[#DFDFDF]'
             } ${idx % 2 === 0 ? `rounded-l` : 'rounded-r'}`}
             onClick={() => setSelected(option.value)}
