177acaa2001f855d60680419456b22a12271117c
shaddamalbz <shaddam.albz@gmail.com>
2024-01-30 13:40:25 +0700

    docs: 0.0.485

--- apps/alurkerja-docs/app/docs/changelog/v0/V485.tsx
+++ apps/alurkerja-docs/app/docs/changelog/v0/V485.tsx
@@ -6,6 +6,26 @@ export default function V485() {
     <SectionLayout title="0.0.484 - 0.0.485" description="penambahan fitur pada komponen">
       <ul>
         <li>terdapat ref untuk mendapatkan current data di tableLowcode `ref.current.getData()`</li>
+        <li>
+          Caranya
+          <pre>
+            <code className="language-tsx">{`const tableRef = useRef(null)`}</code>
+          </pre>
+          <pre>
+            <code className="language-tsx">
+              {`<TableLowcode
+  ref={tableRef}
+/>`}
+            </code>
+          </pre>
+          <pre>
+            <code className="language-tsx">{`console.log(tableRef.current.getData())`}</code>
+          </pre>
+        </li>
+        <p>
+          Catatan: pada saat pertama kali halaman di muat, ref tidak akan langsung memiliki nilai, perlu menunggu
+          komponen selesai di render baru nilai ref terisi
+        </p>
       </ul>
     </SectionLayout>
   )
